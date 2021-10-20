96a807b3350a0c11c73ac9e3bccc5a0550f55baf
AnkithDaMaggiMan <89075382+Krupaankith@users.noreply.github.com>
2021-10-20 12:17:35 +0530

    First Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 2.0f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+    }
+}
